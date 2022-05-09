2e7f7d6b92f9136967bbd91918a54ef50395fd81
최세열 <csy7812@naver.com>
2022-05-10 00:11:23 +0900

    컴포넌트 & 동적인 ui (모달)

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,22 +7,23 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick={ ()=>{
+      <button onClick={()=>{
         let copy = [...글제목];
         copy[0] = '여자 코트 추천';
         글제목변경(copy);
-      } }>글수정</button>
-      <button onClick={ ()=>{
+      }}>글수정</button>
+      <button onClick={()=>{
         let copy = [...글제목];
         copy.sort();
         글제목변경(copy);
-      } }>가나다순</button>
+      }}>가나다순</button>
       <div className="list">
         <h4>{ 글제목[0] }<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span>{ 따봉 }</h4>
         <p>5월 9일 발행</p>
@@ -32,11 +33,28 @@ function App() {
         <p>5월 9일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{
+          if(modal == false) setModal(true);
+          else setModal(false);
+        }}>{ 글제목[2] }</h4>
         <p>5월 9일 발행</p>
       </div>
+
+      {
+        modal == true ? <Modal/> : null
+      } 
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;

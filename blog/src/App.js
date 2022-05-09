ea45a062d018176e990f741f70946f1ce235fedc
최세열 <csy7812@naver.com>
2022-05-09 23:06:52 +0900

    day 1

---
+++ blog/src/App.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <button onClick={ ()=>{
+        let copy = [...글제목];
+        copy[0] = '여자 코트 추천';
+        글제목변경(copy);
+      } }>글수정</button>
+      <button onClick={ ()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      } }>가나다순</button>
+      <div className="list">
+        <h4>{ 글제목[0] }<span onClick={ ()=>{ 따봉변경(따봉+1) } }>👍</span>{ 따봉 }</h4>
+        <p>5월 9일 발행</p>
+      </div> 
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>5월 9일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>5월 9일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;

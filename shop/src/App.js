4fcab2f59cfc3b0d145eddfbd25783f66abb4738
최세열 <csy7812@naver.com>
2022-05-25 01:02:54 +0900

    쇼핑몰 만들기 프로제트 두번째 커밋

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,17 +4,22 @@ import { Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import './App.css';
 import data from './data.js';
 import Detail from './pages/detail.js';
+import axios from 'axios';
 //import bg from './img/bg.png';
 
 // inline 스타일로 배경이미지 넣을 때 : <div className="main-bg" style={{ backgroundImage : 'url(' + bg +')'}}></div>
 // public 폴더 안에 파일은 압축되지 않음
 // publc 폴더 이미지 쓰는 권장방식 : <img src={process.env.PUBLIC_URL + '/logo192.png'} /> 
 // 페이지 이동도와주는 useNavigate()
-// navigate(1), navigage(-1)
+// navigate(1):앞으로 한번 가기, navigage(-1) : 뒤로 1번 가기
+
+// ajax 사용하면 새로고침 없이도 GET/POST요청 가능
+// ajax 쓰려면 옵션 3개 중 택1  : 1.XMLHttpRequest 2.fetch() 3.axios(엑시오스) : npm install axios
+// POST 요청하는 법 : axios.post('URL', {name : 'choi'}) => 이거 실행하면 서버로 {name : 'choi'} 자료가 전송된다.
 
 function App() {
 
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
 
   return (
@@ -49,12 +54,27 @@ function App() {
                     }
                 </Row>
             </Container>
+            <button onClick={()=>{
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((결과)=>{ //결과는 실제로 가져온 데이터
+                let copy = [...shoes, ...결과.data]; // shoes에 가져온 데이터 추가
+                setShoes(copy);
+              })
+              .catch(()=>{ 
+                console.log('실패')
+              }) // ajax 코드가 실패했을 때
+
+              Promise.all([axios.get('/url1'), axios.get('/url2')])
+              .then(()=>{
+
+              })
+            }}>더보기</button>
           </>
         } />
         <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
 
         {/* 
-          Nested Routes
+          Nested Routes : <Route>안에 <Route>를 넣을 수 있는데 이것을 Nested Routes라고 부른다.
           nested routes의 element 보여주는 곳은 <Outlet>
          */}
         <Route path="/about" element={<About/>}>
--- shop/src/pages/detail.js
+++ shop/src/pages/detail.js
@@ -1,25 +1,66 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-// 유저가 URL파라미터에 입력한거 가져오려면 useParams()
-// arrow function에서 return과 중괄호는 동시에 생략가능
+// 컴포넌트의 Lifecyle (중간중간 코드실행 가능)
+// - 페이지에 장착 : mount
+// - 가끔 업데이트 : update
+// - 필요없으면 제거 : unmount
+
+// Side Effect : 함수의 핵심기능과 상관없는 부가기능
 
 function Detail(props) {
+ 
+    let {id} = useParams();
+//  유저가 URL파라미터에 입력한거 가져오려면 useParams()
 
-    let {id} = useParams(); 
     let 찾은상품 = props.shoes.find((x)=>x.id == id);
+//  let 찾은상품 = props.shoes.find(function(x){ return x.id == id }); 
+//  array자료.find(()=>{ return 조건식}) 이렇게 쓰면 조건식에 맞는 자료를 찾아서 이 자리에 남겨준다.
+//  find() 콜백함수에 파라미터 넣으면 array자료에 있던 자료를 뜻한다. 
+//  조건식 x.id == id : array자료.id == url에 입력한 번호 일 경우 결과를 변수에 담아준다.
+//  arrow function에서 return과 중괄호는 동시에 생략가능
 
+    let [alert, setalert] = useState(true);
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setalert(false) }, 2000)
+        
+        return ()=>{
+            clearTimeout(a) //기존타이머는 제거
+        } // clean up function: useEffect 동작 전에 실행, (참고) mount시 실행안됨/unmount시 실행됨
+    }, [])
+    // mount,update시 코드 실행해주는 useEffect
+
+    // useEffect 안에 있는 코드는 html 렌더링 후에 동작한다.
+    // useEffect 안에 적는 코드들은 어려운 연산, 서버에서 데이터가져오는 작업, 타이머 장착하는거
+    // useEffect 실행조건 넣을 수 있는 곳은 []
+
+    // useEffect(()=>{ })     1.재렌더링마다 코드실행하고 싶으면
+    // useEffect(()=>{ }, []) 2.mount시 1회 코드실행하고 싶으면
+    // useEffect(()=>{
+    //      return ()=>{ 3.unmount시 1회 코드 실행하고 싶으면}
+    //}, [])
+    // useEffect(()=>{ }, [state명]) 4.특정 state 변경 시에만 실행하려면 [state명]
+    
     return (
         <div className="container">
+            {
+                alert == true ?
+                <div className="alert alert-warning">
+                    2초이내 구매시 할인
+                </div>
+                : null
+            }
             <div className="row">
-            <div className="col-md-6">
-                <img src={'https://codingapple1.github.io/shop/shoes' + (찾은상품.id+1) +'.jpg'} width="100%" alt="" />
-            </div>
-            <div className="col-md-6">
-                <h4 className="pt-5">{ 찾은상품.title }</h4> 
-                <p>{ 찾은상품.content }</p>
-                <p>{ 찾은상품.price }</p>
-                <button className="btn btn-danger">주문하기</button> 
-            </div>
+                <div className="col-md-6">
+                    <img src={'https://codingapple1.github.io/shop/shoes' + (찾은상품.id+1) +'.jpg'} width="100%" alt="" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="pt-5">{ 찾은상품.title }</h4> 
+                    <p>{ 찾은상품.content }</p>
+                    <p>{ 찾은상품.price }</p>
+                    <button className="btn btn-danger">주문하기</button> 
+                </div>
             </div>
         </div> 
     )
